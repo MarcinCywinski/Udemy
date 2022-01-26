855295a93e79e076f447b94e71f79770abf88ba1
MarcinCywinski <79472045+MarcinCywinski@users.noreply.github.com>
2022-01-26 14:36:14 +0100

    Started JS

---
+++ 2.10/main01.js
@@ -0,0 +1,14 @@
+console.log("ok w znaczniku script");
+
+// Zmienne
+
+let userName = "Marcin";
+let userAge = 18;
+let drivingLicence = true;
+let za5Lat = userAge + 5;
+// let userAge = 29;
+
+var a = 1;
+var a = 2;
+
+console.log(userAge, userName, a, drivingLicence);
