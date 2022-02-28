8bece0d3eb5c415a0e9a357293b8585f5f13691d
MarcinCywinski <79472045+MarcinCywinski@users.noreply.github.com>
2022-02-28 11:04:46 +0100

    Another brick in the wall

---
+++ 2.12/main2.js
@@ -0,0 +1,47 @@
+// console.log($('*'));
+// console.log($('.item1').text());
+
+// $(document).ready(function(){
+//     console.log($('*'));
+//     console.log($('*').length);
+// })
+
+// $(function(){
+//     console.log($('*'));
+//     console.log($('*').length);
+    
+// })
+// console.log($('p').text());
+
+// const $p = $('p');
+// console.log($p.html());
+
+// 2
+
+// const paragraphText = $('p').text();
+// console.log(paragraphText);
+// $('p').text("nowy tekst");
+// console.log($('html').html());
+// console.log($('section').html());
+// console.log($('p:nth-of-type(1)').html());
+// console.log($('p:nth-of-type(1)').text());
+
+// Tworzenie elementu
+
+// const $titleH1 = $('<h1>Nuknuk</h1>');
+// $('div').before($titleH1);
+// $('div').after($titleH1);
+// $('div').prepend($titleH1);
+// $('div').append($titleH1);
+// $titleH1.prependTo('div');
+// $titleH1.appendTo('div');
+
+// 3
+// console.log($('div.item1').attr("class"));
+// const divClass = $('div.item1').attr("class");
+// console.log(divClass);
+
+// console.log($('div.item1').attr("class", "red blue"));
+
+
+
