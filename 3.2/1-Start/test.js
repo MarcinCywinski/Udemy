b41702969b37667cf95551481f502daecd71b3a1
MarcinCywinski <79472045+MarcinCywinski@users.noreply.github.com>
2022-05-31 18:13:40 +0200

    Working on JS

---
+++ 3.2/1-Start/test.js
@@ -0,0 +1,44 @@
+// 00
+console.log("production");
+console.log("apparatus");
+console.log("suburb");
+console.log("lily");
+console.log("bend");
+console.log("stall");
+console.log("mirror");
+console.log("station");
+console.log("contract");
+console.log("dentist");
+// 01
+console.log("------------------------------------------------");
+console.log(2+3/4)
+console.log("23"+"10")
+console.log(234 + (23 % 3))
+console.log(234 * 24)
+// 02
+console.log("------------------------------------------------");
+let village = "Village has 10 people";
+console.log(village);
+// 03
+console.log("------------------------------------------------");
+const appoint = 234 + 2344 * 100;
+console.log(appoint)
+// 04
+console.log("------------------------------------------------");
+const numberit = [23, 24, 45, 56, 67];
+
+console.log(numberit[1]);
+console.log(numberit[1] + numberit[3]);
+
+const a = 4;
+const b = 6;
+
+
+
+
+
+
+
+
+
+
